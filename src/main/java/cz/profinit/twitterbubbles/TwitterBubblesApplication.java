21db988e06189699a945c6380f3b2797ac2b8bb4
Tomáš Piňos <tomas.pinos@profinit.eu>
2018-03-23 10:29:01 +0100

    First skeleton created by Spring Initializr.

---
+++ src/main/java/cz/profinit/twitterbubbles/TwitterBubblesApplication.java
@@ -0,0 +1,12 @@
+package cz.profinit.twitterbubbles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterBubblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterBubblesApplication.class, args);
+	}
+}
