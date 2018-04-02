6131fb22b9095e0e099dfd4c7ca0e1a62d501189
Jonas Klimes <jonas.klimes@profinit.eu>
2018-04-02 18:29:20 +0200

    Add build and git info to /actuator/info.

--- server/src/main/java/cz/profinit/twitterbubbles/TwitterBubblesApplication.java
+++ server/src/main/java/cz/profinit/twitterbubbles/TwitterBubblesApplication.java
@@ -2,8 +2,10 @@ package cz.profinit.twitterbubbles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:git.properties")
 public class TwitterBubblesApplication {
 
 	public static void main(String[] args) {
