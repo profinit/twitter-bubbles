e9be9ffff237afd6fcb28cff064215d32775d5bf
Petr Janik <xjanik5@fi.muni.cz>
2019-10-23 18:49:01 +0200

    implement words split and word count

--- server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
+++ server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
@@ -4,8 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
 
 @Component
 public class TweetProcessor {
@@ -23,7 +28,21 @@ public class TweetProcessor {
 
         // TODO Rozdělení textu do slov a spočítání počtu jejich výskytů.
         // TODO Implementace je hotová, pokud uspěje unit test TweeProcessorTest.
+        String[] split = text.toLowerCase().replaceAll("\\W", " ").replaceAll("\\s+", " ").split(" ");
+        Set<String> filteredWords = new HashSet<>(Arrays.asList("https", "japan"));
+        Map<String, Integer> map = Arrays.stream(split).filter(word -> !filteredWords.contains(word)).collect(groupingBy(word -> word, reducing(0, e -> 1, Integer::sum)));
 
-        return Collections.emptyMap();
+//        for (String s : split) {
+//            if ("https".equals(s)){
+//                continue;
+//            }
+//            if (!map.containsKey(s)) {
+//                map.put(s, 1);
+//            } else {
+//                int count = map.get(s);
+//                map.put(s, count + 1);
+//            }
+//        }
+        return map;
     }
 }
