70c66192dd33f8acf569a34f901febd912d66387
Tjev <tomas.jevocin@gooddata.com>
2019-10-23 19:02:00 +0200

    Configure API keys and port

--- server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
+++ server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
@@ -4,13 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class TweetProcessor {
 
     private static final Logger log = LoggerFactory.getLogger(TweetProcessor.class);
+
+    private String wordParser(String text) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isLetterOrDigit(c) || c == '_') {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 
     /**
      * Splits the provided text into words and counts their occurrences.
@@ -24,6 +37,20 @@ public class TweetProcessor {
         // TODO Rozdělení textu do slov a spočítání počtu jejich výskytů.
         // TODO Implementace je hotová, pokud uspěje unit test TweeProcessorTest.
 
-        return Collections.emptyMap();
+        Map<String, Integer> counts = new HashMap<>();
+
+        for (String word : text.split(" ")) {
+            String cleanWord = wordParser(word);
+            if (cleanWord.length() > 0) {
+                String lowerCaseWord = cleanWord.toLowerCase();
+                if (counts.containsKey(lowerCaseWord)) {
+                    counts.put(lowerCaseWord, counts.get(lowerCaseWord) + 1);
+                } else {
+                    counts.put(lowerCaseWord, 1);
+                }
+            }
+        }
+
+        return counts;
     }
 }
