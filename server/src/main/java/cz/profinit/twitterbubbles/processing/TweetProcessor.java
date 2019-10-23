08cdd2a2220189d6b0d04e959290cc4f8300de08
@xdemcak <@xdemcak>
2019-10-23 18:54:20 +0200

    processTweet

--- server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
+++ server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -20,10 +21,20 @@ public class TweetProcessor {
      */
     public Map<String, Integer> processTweetText(String text) {
         log.trace("Processing tweet");
+        Map <String, Integer> map = new HashMap();
 
         // TODO Rozdělení textu do slov a spočítání počtu jejich výskytů.
+
+        String[] words = text.toLowerCase().split(" ");
+        for ( String word  : words){
+            word = word.replaceAll("[\\W]|_", "");
+            //word = word.replaceAll("\\.", "");
+            if (!word.equals("")){
+                map.merge(word, 1,(a,b) -> a+b);
+            }
+        }
         // TODO Implementace je hotová, pokud uspěje unit test TweeProcessorTest.
 
-        return Collections.emptyMap();
+        return map;
     }
 }
