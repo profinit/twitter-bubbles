18cdc4041d1f5d874ed74f2e1ff1e92064cc4add
Hai Yen <456181@mail.muni.cz>
2019-10-23 18:57:00 +0200

    process tweet text

--- server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
+++ server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -18,12 +19,38 @@ public class TweetProcessor {
      * @param text Tweet text.
      * @return Word to occurrence count map. When no words founds, the method should return an empty map, but never {@code null}.
      */
+
+    private static String getWord(String string){
+        String result = "";
+        for(int i = 0; i < string.length(); i++ ){
+            if(Character.isLetter(string.charAt(i)) || Character.isDigit(string.charAt(i))){
+                result += string.charAt(i);
+            }
+        }
+        return result.toLowerCase();
+    }
+
     public Map<String, Integer> processTweetText(String text) {
         log.trace("Processing tweet");
+
+        String[] res = text.split(" ", 100);
+
+
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for(String item : res){
+            String s = getWord(item);
+            if(!map.containsKey(s)){
+                map.put(s, 1);
+            } else{
+                map.put(s, map.get(s) + 1);
+
+            }
+        }
 
         // TODO Rozdělení textu do slov a spočítání počtu jejich výskytů.
         // TODO Implementace je hotová, pokud uspěje unit test TweeProcessorTest.
 
-        return Collections.emptyMap();
+        return map;
     }
 }
