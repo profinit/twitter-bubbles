c6b43de6b7e21bf4658d3cc184f67a642138a598
Rudolf Kucera <rudi1217@gmail.com>
2019-10-23 18:51:24 +0200

    changed TweetProcessor.java

--- server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
+++ server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
@@ -4,8 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class TweetProcessor {
@@ -21,9 +20,24 @@ public class TweetProcessor {
     public Map<String, Integer> processTweetText(String text) {
         log.trace("Processing tweet");
 
+        Map <String, Integer> result = new HashMap<>();
+
+        text = text.toLowerCase();
+        text = text.replaceAll("[^a-zA-Z0-9]", " ");
+        String[] words = text.split(" ");
+
+        for (String word : words)
+        {
+            if (!word.equals("") && !word.equals("https"))
+                if (result.containsKey(word))
+                    result.put(word, result.get(word)+1);
+                else
+                    result.put(word, 1);
+        }
+
         // TODO Rozdělení textu do slov a spočítání počtu jejich výskytů.
         // TODO Implementace je hotová, pokud uspěje unit test TweeProcessorTest.
 
-        return Collections.emptyMap();
+        return result;
     }
 }
